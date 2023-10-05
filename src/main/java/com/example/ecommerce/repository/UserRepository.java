7c270667bcd78c7241ee2b249e646987bf1fe2cc
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-05 18:43:42 -0300

    Criada a classe UserRepository

---
+++ src/main/java/com/example/ecommerce/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+    public Users findByEmail(String email);
+}
