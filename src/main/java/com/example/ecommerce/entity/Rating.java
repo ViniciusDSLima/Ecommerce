9e2552d1f16734bd0ac1ee0bc9c8569d2219b3e3
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-05 18:20:15 -0300

    Criada a tabela Rating

---
+++ src/main/java/com/example/ecommerce/entity/Rating.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users users;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private double rating;
+}
